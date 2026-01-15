fb479ea6e741a23156a5d40ba806f8d07d10e67f
Alena Rossi <abross28@colby.edu>
2026-01-15 00:11:25 +0000

    Homework 1

--- functions/observations.R
+++ functions/observations.R
@@ -1,9 +1,31 @@
 
-read_observations = function(scientificname = "Orcinus orca",
+```{r}
+read_observations = function(scientificname = "Squalus acanthias",
+                             minimum_year = 1970){
+  x = read_obis(scientificname, "Squalus acanthias") |>
+    dplyr::mutate(month = factor(month, levels = month.abb))}
+```
+
+read_observations = function(scientificname = "Squalus acanthias",
                              minimum_year = 1970){
   # read in the raw data
   x = read_obis(scientificname, ...) |>
     dplyr::mutate(month = factor(month, levels = month.abb))}
+```{r}
+obs = obs |>
+  mutate(month = factor(month, levels = month.abb))
+
+ggplot(data = obs,
+       mapping = aes(x = month)) + 
+  geom_bar() + 
+  labs(title = "Counts per month")
+
+dplyr::mutate(month = factor(month, levels = month.abb))
+if (!is.null(individualCount))
+  x<-x |>
+      filter(count >= individualCount)
+
+```
   
   
   # if the user provided a non-NULL filter by year
@@ -22,7 +44,7 @@ read_observations = function(scientificname = "Orcinus orca",
   # Happy coding!
   
  
-  read_observations = function(scientificname = "Orcinus orca",
+  read_observations = function(scientificname = "Squalus acanthias",
                                                               indiviudalCount=10,...) {
                                 
          x = read_obis(scientificname, ...) |>
@@ -32,4 +54,12 @@ read_observations = function(scientificname = "Orcinus orca",
                 filter(count >= individualCount)
           return(x)
   }
+```{r}
+ x = read_obis(scientificname) |>
+           dplyr::mutate(month = factor(month, levels = month.abb))
+       if (!is.null(individualCount)) {
+    x <- x |> dplyr::filter(count >= individualCount)
+  }
+```
+  
   
